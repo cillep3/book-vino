6b79ac53f991e12d9489126e2bb90fd0ac41ea86
anebundgaard <anebundgaard95@gmail.com>
2021-06-15 12:59:46 +0200

    Listelement

--- src/components/Listelement.jsx
+++ src/components/Listelement.jsx
@@ -7,28 +7,28 @@ import Register from '../helpers/register.json'
 const Listelement = () => {
     return (
         <div id="listContainer">
-            <figure id="listElement">
+            <figure>
                 {
-                    <>
-                    <img id="hero" src={Register.winery[0].image} alt="Marchesini Marcello" />
-                    <h1>{Register.winery[0].title}</h1>
-                    <img id="stars" src={Register.winery[0].stars} alt="Marchesini Marcello" />
-                    {/* <div>
-                        {Register.winery[0].icons[0].vector}
-                    </div> */}
-                    <div id="icons">
-                        {Register.winery[0].icons.map((data, index) =>
-                            <img src={data.vector}/>
-                        )}
-                        {Register.winery[0].icons.map((data, index) =>
-                            <p>{data.text}</p>
-                        )}
+                    <div id="listElement">
+                        <img id="hero" src={Register.winery[0].image} alt="Marchesini Marcello" />
+                        <h1>{Register.winery[0].title}</h1>
+                        <img id="stars" src={Register.winery[0].stars} alt="Marchesini Marcello" />
+                        {/* <div>
+                            {Register.winery[0].icons[0].vector}
+                        </div> */}
+                        <div id="icons">
+                            {Register.winery[0].icons.map((data, index) =>
+                                <img src={data.vector}/>
+                            )}
+                            {Register.winery[0].icons.map((data, index) =>
+                                <p>{data.text}</p>
+                            )}
+                        </div>
                     </div>
-                   
-                    </>
                 }
+                <button>Get more information</button>
             </figure>
-            <button>Get more information</button>
+            
         </div>
     )
 }
