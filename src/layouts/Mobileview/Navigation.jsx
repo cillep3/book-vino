b57248b49ed40127ac36acb964f3019c928deda5
cillep3 <cillep3@hotmail.com>
2021-06-16 22:37:45 +0200

    mobile nav

--- src/layouts/Mobileview/Navigation.jsx
+++ src/layouts/Mobileview/Navigation.jsx
@@ -1,17 +1,53 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import '../Mobileview/Navigation.scss'
 
-const Navigation = () => {
+import { makeStyles, withTheme } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+
+
+import Home from '@material-ui/icons/HomeRounded';
+import AboutUs from '@material-ui/icons/FavoriteRounded';
+import Login from '@material-ui/icons/PersonRounded';
+import Wineries from '@material-ui/icons/LocationOnRounded';
+
+
+
+const useStyles = makeStyles({
+    root: {
+        width: 490,
+        paddingTop: 30,
+        paddingBottom: 50,
+
+    },
+});
+
+export default function SimpleBottomNavigation() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
     return (
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/adminWinery">Admin</NavLink>
-            <NavLink to="/editWineries">Edit</NavLink>
-            <NavLink to="/listelement">Listelement</NavLink>
-            <NavLink to="/allwinerys">Allwinerys</NavLink>
-            <NavLink to="/aboutus">About</NavLink>
-        </nav >
-    )
-}
+        <BottomNavigation
+            className={classes.navbar}
+            value={value}
+            onChange={(event, newValue, onChange) => {
+                setValue(newValue);
+            }}
+            showLabels
+            className={classes.root}
+            id="bottom-nav"
+        >
+            <BottomNavigationAction component={Link} to={'/'} value={'/'} label={'Home'} icon={<Home />} />
+            <BottomNavigationAction component={Link} to={'/allwinerys'} value={'/allwinerys'} label={'Wineries'} icon={<Wineries />} />
 
-export default Navigation
+
+            <BottomNavigationAction component={Link} to={'/aboutus'} value={'/aboutus'} label={'About Us'} icon={<AboutUs />} />
+
+            <BottomNavigationAction component={Link} to={'/adminWinery'} value={'/adminWinery'} label={'Login'} icon={<Login />} />
+
+
+        </BottomNavigation>
+    );
+}
--- src/pages/Mobileview/mobile_pages/Home.jsx
+++ src/pages/Mobileview/mobile_pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../../Mobileview/Mobile.scss'
 
 // import of components
@@ -9,7 +9,7 @@ import Register from '../../../helpers/register.json'
 const Home = () => {
     return (
         <div id="home">
-            <img src={Register.images[4].src} />
+            <img className='hero' src={Register.images[4].src} />
             <h1>Our Wineries</h1>
             <Liste />
             <Link to="/allwinerys"><button id="homeButton">Load more</button></Link>
