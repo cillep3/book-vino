cd417ec7d83699154c4cbf6981b9a80d04592d45
anebundgaard <anebundgaard95@gmail.com>
2021-06-16 09:30:23 +0200

    Liste

--- src/components/Listelement.jsx
+++ src/components/Listelement.jsx
@@ -11,11 +11,10 @@ const Listelement = () => {
                 {
                     <div id="listElement">
                         <img id="hero" src={Register.winery[0].image} alt="Marchesini Marcello" />
-                        <h1>{Register.winery[0].title}</h1>
-                        <img id="stars" src={Register.winery[0].stars} alt="Marchesini Marcello" />
-                        {/* <div>
-                            {Register.winery[0].icons[0].vector}
-                        </div> */}
+                        <div id="borderBox">
+                            <h1>{Register.winery[0].title}</h1>
+                            <img id="stars" src={Register.winery[0].stars} alt="Marchesini Marcello" />
+                        </div>
                         <div id="icons">
                             {Register.winery[0].icons.map((data, index) =>
                                 <img id="icon_image" src={data.vector}/>
@@ -28,7 +27,26 @@ const Listelement = () => {
                 }
                 <button>Get more information</button>
             </figure>
-            
+            <figure>
+                {
+                    <div id="listElement">
+                        <img id="hero" src={Register.winery[0].image} alt="Marchesini Marcello" />
+                        <div id="borderBox">
+                            <h1>{Register.winery[0].title}</h1>
+                            <img id="stars" src={Register.winery[0].stars} alt="Marchesini Marcello" />
+                        </div>
+                        <div id="icons">
+                            {Register.winery[0].icons.map((data, index) =>
+                                <img id="icon_image" src={data.vector}/>
+                            )}
+                            {Register.winery[0].icons.map((data, index) =>
+                                <p id="icon_text">{data.text}</p>
+                            )}
+                        </div>
+                    </div>
+                }
+                <button>Get more information</button>
+            </figure>
         </div>
     )
 }
