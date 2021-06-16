c415e16514c03fa1cc84969aeb2345111574f61a
anebundgaard <anebundgaard95@gmail.com>
2021-06-16 12:12:12 +0200

    Mappestruktur

--- src/pages/Mobileview/Index.jsx
+++ src/pages/Mobileview/Index.jsx
@@ -11,6 +11,13 @@ import About from '../Mobileview/mobile_pages/About'
 import Admin from './mobile_pages/AdminWinery'
 import Edit from './mobile_pages/EditWinery'
 
+// Import of single wineries components
+import Corte from './mobile_pages/single_wineries/Corte_Archi'
+import Marognole from './mobile_pages/single_wineries/Le_Marognole'
+import Marchesini from './mobile_pages/single_wineries/Marchesini_Marcello'
+import Marco from './mobile_pages/single_wineries/Marco_Reggio'
+import Montalto from './mobile_pages/single_wineries/Sol_de_Montalto'
+
 const Index = () => {
     return (
         <div id="mobileContainer">
@@ -31,6 +38,21 @@ const Index = () => {
                     <Admin />
                 </Route>
                 <Route path="/editWineries/:id" component={Edit} />
+                <Route path="/corte-archi">
+                    <Corte/>
+                </Route>
+                <Route path="/le-marognole">
+                    <Marognole/>
+                </Route>
+                <Route path="/marchesini-marcello">
+                    <Marchesini/>
+                </Route>
+                <Route path="/marco-reggio">
+                    <Marco/>
+                </Route>
+                <Route path="/sol-de-montalto">
+                    <Montalto/>
+                </Route>
                     
             </section>
 
