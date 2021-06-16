9726682f7e217786a0290818ca90d55664053855
mhebsgaard <minihebs@live.dk>
2021-06-16 11:43:18 +0200

    About

--- src/pages/Mobileview/mobile_pages/About.jsx
+++ src/pages/Mobileview/mobile_pages/About.jsx
@@ -1,12 +1,13 @@
 import Register from "../../../helpers/register.json";
 import React from "react";
+import '../../Mobileview/Mobile.scss'
 
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <img src={Register.images[0].src} />
       <h1>About BookVino</h1>
-      <p>
+      <h4>
         We help you find authentic wine experiences to bring you the best
         memories of your vacation home. At many wineries you can take a quick
         look around and get a free taste before buying a few bottles. But you
@@ -16,17 +17,23 @@ const About = () => {
         about the production and an extended tasting. At BookVino.com you will
         only find less family owned and authentic wineries, where we are sure
         you will have a really good holiday experience.
-      </p>
+      </h4>
       <h1>Booking Process</h1>
-      <img />
+      <div className="yellowcircle">
+      <img src={Register.images[1].src} />
+      </div>
       <h2>Go to webpage</h2>
       <p>Go to our webpage and find the winerie you want to visit.</p>
-      <img />
+      <div className="yellowcircle">
+      <img src={Register.images[2].src} />
+      </div>
       <h2>Book wine tasting</h2>
       <p>Book your tasting directly through the wineries</p>
-      <img />
+      <div className="yellowcircle">
+      <img src={Register.images[3].src} />
+      </div>
       <h2>Enjoy your tasting</h2>
-      <p>Book your tasting directly through the wineries</p>
+      <p>Now you receive your booking details in your mailboks. Enjoy your tasting!!</p>
     </div>
   );
 };
