317f72ac4529da30e951bddecaf5652e95246b45
mhebsgaard <minihebs@live.dk>
2021-06-16 10:43:44 +0200

    Update About.jsx

--- src/pages/Mobileview/About.jsx
+++ src/pages/Mobileview/About.jsx
@@ -1,10 +1,13 @@
+import Register from '../../helpers/register.json'
 import React from 'react'
+
 
 
 const About = () => {
     return (
         <div>
-            
+            <img src={Register.images[0].src}/>
+            <h1>About BookVino</h1>
         </div>
     )
 }
