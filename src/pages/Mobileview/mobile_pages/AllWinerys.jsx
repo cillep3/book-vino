68c5974c7959dc59dbdedcb3edccab8403e1f329
mhebsgaard <minihebs@live.dk>
2021-06-17 10:26:19 +0200

    Update AllWinerys.jsx

--- src/pages/Mobileview/mobile_pages/AllWinerys.jsx
+++ src/pages/Mobileview/mobile_pages/AllWinerys.jsx
@@ -37,19 +37,25 @@ const AllWinerys = (props) => {
     }, [byNavne])
 
     return (
-
-
-        {
-            AllWinerys &&
-        <div id="kort">
-            <Kort koordinater={[AllWinerys.coord.lat, AllWinerys.coord.lon]} />
-        </div>
+        <>
+            <div id="kortContainer">
+                <h1>{byNavne} </h1>
+                <div id="inputfelt">
+                    <input className="inputdesign" type="text" placeholder="Choose Area" onChange={(e) => setbyNavne(e.target.value)} />
+                    <img id="loupeicon" src={Register.icons[0].src} />
+                </div>
+ 
+                {
+                    AllWinerys &&
+                    <div id="kort">
+                        <Kort koordinater={[AllWinerys.coord.lat, AllWinerys.coord.lon]} />
+                    </div>
                 }
-            </div >
+            </div>
             <h1 id="allWineriesHeadline">Wineries</h1>
             <Liste />
         </>
     )
 }
-
+ 
 export default AllWinerys;
