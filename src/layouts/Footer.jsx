4808b86ec215162ba6f56d360fc2aa0513dda9e5
cillep3 <cillep3@hotmail.com>
2021-06-17 13:30:55 +0200

    icon

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import '../pages/Mobileview/Mobile.scss'
 import Register from '../helpers/register.json'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+import { FaMobileAlt } from 'react-icons/fa';
+
+
 
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="footlogo"><img src={Register.images[8].src}/></div>
+            <div className="footlogo"><img src={Register.images[8].src} /></div>
             <div id="contact">
-                <img src={Register.images[6].src}/>
+                <div className="iconfa"><FaEnvelope /></div>
                 <p>ck@bookvino.com</p>
             </div>
             <div id="contact">
-                <img src={Register.images[5].src}/>
+                <div className="iconfa"><FaMapMarkerAlt /></div>
                 <p>Engdalen 28, 8500 Grenaa</p>
             </div>
             <div id="contact">
-                <img src={Register.images[7].src}/>
+                <div className="iconfa"><FaMobileAlt /></div>
+
                 <p>+45 29875722</p>
             </div>
             <button>BOOK NOW</button>
             <h5>FAQ</h5>
             <Link to="/aboutus"><h5>About BookVino</h5></Link>
             <Link to="/allwinerys"><h5>Wineries</h5></Link>
-            <img alt=""/><img alt=""/><img alt=""/>
+            <img alt="" /><img alt="" /><img alt="" />
             <h6>Copyright © bookvino.com 2020 All Rights Reserved<br></br>CVR: 37992240</h6>
-            
+
         </div>
     )
 }
