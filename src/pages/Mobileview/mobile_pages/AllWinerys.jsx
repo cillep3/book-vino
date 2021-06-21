caac9f8bd4a8d1a86f082657599efe186e05c854
anebundgaard <anebundgaard95@gmail.com>
2021-06-21 09:10:57 +0200

    Wineries

--- src/pages/Mobileview/mobile_pages/AllWinerys.jsx
+++ src/pages/Mobileview/mobile_pages/AllWinerys.jsx
@@ -7,6 +7,7 @@ import {Grid} from "@material-ui/core"
 import TextField from "@material-ui/core/TextField"
 import Container from "@material-ui/core/Container"
 import Register from "../../../helpers/register.json"
+import Liste from '../../../components/Listelement'
 
 
 
@@ -65,48 +66,35 @@ const AllWinerys = (props) => {
     }
 
     return (
+        <>
+            <div id="kortContainer">
+            <div id="textField" onChange={(e) => setbyNavne(e.target.value)}>
+                    <TextField 
+                        variant="outlined"
+                        color="standard"
+                        label="Choose Area"
+                        size="small"
+                    />
+            </div>
+                {
+                    // Her kommer Winerydata ind når man trykker. 
+                    AllWinerys &&
+                    <div>
+                        <h1>{AllWinerys.name} </h1>
+                    </div>
+                }
+                {
+                    AllWinerys &&
+                    <div id="kort">
+                        <Kort koordinater={[AllWinerys.coord.lat, AllWinerys.coord.lon]} />
+                    </div>
 
-        
-        <div id="kortContainer">
-           
-           <div id="textField" onChange={(e) => setbyNavne(e.target.value)}>
+                }
+            </div>
 
-                <TextField 
-                    variant="outlined"
-                    color="standard"
-                    label="Choose Area"
-                    size="small"
-                />
-           
-           </div>
-         
-
-
-            {/* <div id="inputfelt">
-                <input className="inputdesign" type="text" placeholder="Choose Area" onChange={(e) => setbyNavne(e.target.value)} />
-                <img id="loupeicon"src = {Register.icons[0].src} />
-            </div> */}
-
-
-
-
-            {
-                // Her kommer Winerydata ind når man trykker. 
-                AllWinerys &&
-                <div>
-                    <h1>{AllWinerys.name} </h1>
-                </div>
-            }
-
-            {
-                AllWinerys &&
-                <div id="kort">
-                    <Kort koordinater={[AllWinerys.coord.lat, AllWinerys.coord.lon]} />
-                </div>
-
-            }
-
-        </div>
+            <h1 id="allWineriesHeadline">Wineries</h1>
+            <Liste />
+        </>
 
     )
 }
