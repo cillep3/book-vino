f82c35cdb274ad8f76052f2d185c045b0b9bc06b
anebundgaard <anebundgaard95@gmail.com>
2021-06-23 09:54:33 +0200

    json

--- src/pages/Mobileview/mobile_pages/single_wineries/Corte_Archi.jsx
+++ src/pages/Mobileview/mobile_pages/single_wineries/Corte_Archi.jsx
@@ -6,6 +6,7 @@ import { FaMobileAlt } from 'react-icons/fa';
 import { FaRegEnvelope } from 'react-icons/fa';
 import { FaGlobe } from 'react-icons/fa';
 import { BsClock } from 'react-icons/bs';
+import Slider from '../../../../components/Slider'
 
 /* ======= KORT IMPORT ====== */
 import Kort from "../../../../components/Kort"
@@ -48,7 +49,8 @@ const Corte_Archi = (props) => {
     return (
         <div className="singlewine">
 
-            <img src={Register.winery[4].image} className="topimg" alt="" />
+            {/* Vi kalder på slideren og overfører den respektive sides data til props. I dette tilfælde har vi kaldt data = sliderdata, som rummer 'reggio'-data fra vores register.json. Sliderdata bliver kaldt på i slideren */}
+            <Slider sliderdata={Register.corte_archi} className="topimg" />  
 
             <div className="firstSection">
                 <h1>Corte Archi</h1>
