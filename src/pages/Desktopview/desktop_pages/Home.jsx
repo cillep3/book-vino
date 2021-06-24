7ebdf3dd0d3468f50acf08e1566a2857b4d19f82
cillep3 <cillep3@hotmail.com>
2021-06-24 12:24:30 +0200

    hero

--- src/pages/Desktopview/desktop_pages/Home.jsx
+++ src/pages/Desktopview/desktop_pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../../Mobileview/Mobile.scss'
+import '../Desktop.scss'
+import heroVideo from "../../../assets/vineyard.mp4"
+
+
 
 // import of components
 import Liste from '../../../components/Listelement'
@@ -8,18 +11,23 @@ import Register from '../../../helpers/register.json'
 
 const Home = () => {
     return (
-        <div id="home">
-            <div id="home">
-
-                <div id="hero_box">
-                    <img id="home_hero" src={Register.images[4].src} alt=""/>
-                    <h1>Wine tasting around the world</h1>
-                </div>
+        <div id="homeDesktop">
 
 
-                <h1 id="homeHeadline">Our Wineries</h1>
+            <div className="hero-container">
+
+                <video src={heroVideo} autoPlay loop muted />
+
+                {/* <img src={hero} alt="Hero billede" /> */}
+                <h1>Wine tasting around the world</h1>
+            </div>
+
+            <div className="list">
+                <h1 id="homeHeadlineDesktop">Our Wineries</h1>
+
                 <Liste />
                 <Link to="/allwinerys"><button id="homeButton">Load more</button></Link>
+
             </div>
         </div>
     )
