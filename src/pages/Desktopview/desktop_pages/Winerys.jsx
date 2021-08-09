6c34fce902bb6bea0274a49af92b54a1f94286f8
anebundgaard <anebundgaard95@gmail.com>
2021-08-09 13:13:54 +0200

    fff

--- src/pages/Desktopview/desktop_pages/Winerys.jsx
+++ src/pages/Desktopview/desktop_pages/Winerys.jsx
@@ -2,20 +2,72 @@ import {useEffect, useState} from 'react'
 import Register from '../../../helpers/register.json'
 import Liste from '../../../components/Listelement'
 import '../Desktop.scss'
-import Map from '../../../components/Kort'
+import Kort from '../../../components/Kort'
 import TextField from "@material-ui/core/TextField"
 import { getAllWinerysByZip } from '../../../helpers/apikaldleaflet'
 
-const Winerys = () => {
+const Winerys = (props) => {
+  // 8000 betyder at den viser postnumer fra starten af
+    // alt det som kommer ind i [] vil være navnet på vores data. vejret er classen og setVejret gør at vi kan ændre den.
+
+
+    const [byNavne, setbyNavne] = useState("Rome") // Her er zipcode skiftet ud med "bynavn"
+    const [AllWinerys, setAllWinerys] = useState() // Vindata
+    const [error, setError] = useState() // FEJLBESKEDER
+
+
+
+    // alt det der skal ske, skrives i [] (når denne er loadet hentes vindata)
+    useEffect(() => {
+
+        // setTimeout(() => {
+
+        if (byNavne.length != null) {
+
+            getAllWinerysByZip(byNavne).then(winerydata => {
+
+                console.log(winerydata)
+                setAllWinerys(winerydata); // put vindata i vindata-state
+                setError();          // Tøm fejlbesked
+
+                props.koord([winerydata.coord.lat, winerydata.coord.lon])
+
+                // for at gribe fejlene som kommer tilbage - gemmes i error.
+            }).catch(err => {
+
+                console.log(err)
+                setError(err) // Put fejlbesked i errorstate / ER SERVICE NEDE ELLER HAR DU INDTASTET FORKERT POSTNUMMER
+
+            })
+        }
+
+    }, [byNavne])
+
   return (
-    <div id="allwineriesContainer">
-      <section id="cardContainer">
-        <Liste />
-      </section>
-      <figure id="mapContainer">
-        <h1>map</h1>
-      </figure>
-    </div>
+    <>
+      <div id="allwineriesContainer">
+        <section id="cardContainer">
+          <Liste />
+        </section>
+        <div id="kortContainer">
+                <div id="textField" onChange={(e) => setbyNavne(e.target.value)}>
+                    <TextField
+                        variant="outlined"
+                        color="standard"
+                        label="Choose Area"
+                        size="small"
+                    />
+                </div>
+                {
+                    AllWinerys &&
+                    <div id="kort">
+                        <Kort koordinater={[AllWinerys.coord.lat, AllWinerys.coord.lon]} />
+                    </div>
+
+                }
+            </div>
+      </div>
+    </>
   )
 }
 
