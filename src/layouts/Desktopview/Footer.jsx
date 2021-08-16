5a6bbc80f94e231b7d179cafb2750533c473de21
Raunkilde91 <34437654+Raunkilde91@users.noreply.github.com>
2021-08-16 10:24:10 +0200

    hej

--- src/layouts/Desktopview/Footer.jsx
+++ src/layouts/Desktopview/Footer.jsx
@@ -15,27 +15,35 @@ import { FaYoutubeSquare } from 'react-icons/fa';
 const Footer = () => {
     return (
         <div className="deskfooter">
-            <h5>FAQ</h5>
-            <h5>About BookVino</h5>
-            <h5>Wineries</h5>
+            <div id = "deskLeft">
+                <h5>FAQ</h5>
+                <h5>About BookVino</h5>
+                <h5>Wineries</h5>
+            </div>
+            
             <div className="deskfootlogo"><Link to="/"><img src={Register.images[8].src} /></Link></div>
             <div className="desksocial"><FaFacebookSquare /><FaInstagramSquare /><FaYoutubeSquare /></div>
             <h6>Copyright © bookvino.com 2020 All Rights Reserved<br></br>CVR: 37992240</h6>
-            <div id="deskcontact">
-                <div className="iconfa"><FaEnvelope /></div>
-                <p>ck@bookvino.com</p>
-            </div>
-            <div id="deskcontact">
-                <div className="iconfa"><FaMapMarkerAlt /></div>
-                <p>Engdalen 28, 8500 Grenaa</p>
-            </div>
-            <div id="deskcontact">
-                <div className="iconfa"><FaMobileAlt /></div>
 
-                <p>+45 29875722</p>
-            </div>
+            <div id = "deskContact">
+
+          
+                <p><FaEnvelope fontSize = "25px"  /> ck@bookvino.com</p>
+                
+          
+                <p><FaMapMarkerAlt fontSize = "25px" /> Engdalen 28, 8500 Grenaa</p>
+                
+           
+                <p><FaMobileAlt fontSize = "25px"  /> +45 29875722</p>
+
+                
+            
+        </div>
+
         </div>
     )
 }
+
+
 
 export default Footer
